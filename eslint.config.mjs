99fc69dd740bafc2056a65c54ce61d88baadd96c
André Mathlener <andre.mathlener@ns.nl>
2026-02-09 01:52:02 +0100

    feat: add eslint-plugin-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,21 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     {
+        plugins: {
+            prettier,
+        },
         rules: {
+            'prettier/prettier': 'error',
             'quote-props': ['error', 'consistent-as-needed'],
             '@typescript-eslint/no-explicit-any': 'off',
         },
     },
+    eslintConfigPrettier,
 );
