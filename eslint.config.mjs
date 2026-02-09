6cf9773bb64e240fe58a7191276a5222c229ecb1
André Mathlener <andre.mathlener@ns.nl>
2026-02-09 01:41:41 +0100

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        rules: {
+            'quote-props': ['error', 'consistent-as-needed'],
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+    },
+);
