9d4f7b0c816fdcd738917861e01866a401e546d8
André Mathlener <andre.mathlener@ns.nl>
2026-02-09 02:43:43 +0100

    feat: don't allow relative imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig(
     eslint.configs.recommended,
@@ -12,11 +13,21 @@ export default defineConfig(
     {
         plugins: {
             prettier,
+            import: importPlugin,
+        },
+        settings: {
+            'import/resolver': {
+                typescript: {
+                    alwaysTryTypes: true,
+                },
+            },
         },
         rules: {
             'prettier/prettier': 'error',
             'quote-props': ['error', 'consistent-as-needed'],
             '@typescript-eslint/no-explicit-any': 'off',
+            'import/no-relative-packages': 'error',
+            'import/no-relative-parent-imports': 'error',
         },
     },
     eslintConfigPrettier,
